0d4115793a10852169b5856fccb1208c0c7875f1
mcb3 <matheuscberger@gmail.com>
2018-12-01 18:40:15 -0300

    reorganized the code in separete class

---
+++ bikeMqtt/MqttProvider.swift
@@ -0,0 +1,63 @@
+//
+//  MqttProvider.swift
+//  bikeMqtt
+//
+//  Created by Matheus Coelho Berger on 01/12/18.
+//  Copyright © 2018 vinicius emanuel. All rights reserved.
+//
+
+import Foundation
+import CocoaMQTT
+
+class MqttProvider: CocoaMQTTDelegate {
+    
+    fileprivate var mqtt: CocoaMQTT?
+    
+    fileprivate let host = "broker.hivemq.com"
+    fileprivate let topic = "bikeiot"
+    
+    fileprivate let clientID = "clienteiOSApp"
+    fileprivate let port = 1883
+    
+    init() {
+        self.mqtt = CocoaMQTT(clientID: clientID, host: host, port: UInt16(port))
+        self.mqtt?.delegate = self
+        self.mqtt?.connect()
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        print(ack)
+        self.mqtt?.subscribe(topic)
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
+        print(topic)
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16) {
+        if let message = message.string {
+            print(message)
+        }
+    }
+    
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        print(err.debugDescription)
+        self.mqtt?.connect()
+    }
+
+    
+    ///extra stuff not needed
+    
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+    }
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+    }
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+    }
+}
--- bikeMqtt/ViewController.swift
+++ bikeMqtt/ViewController.swift
@@ -11,59 +11,9 @@ import CocoaMQTT
 
 class ViewController: UIViewController {
     
-    var mqtt: CocoaMQTT?
-    let host = "broker.hivemq.com"
-    let topic = "bikeiot"
-//    let topic = "/topico"
-    let clientID = "clienteiOSApp"
-    let port = 1883
+    var mqttProvider = MqttProvider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configMQTT()
-    }
-    
-    func configMQTT(){
-        
-        self.mqtt = CocoaMQTT(clientID: clientID, host: host, port: UInt16(port))
-        self.mqtt?.delegate = self
-        self.mqtt?.connect()
     }
 }
-
-extension ViewController: CocoaMQTTDelegate{
-    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
-        print(ack)
-        self.mqtt?.subscribe(topic)
-    }
-    
-    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
-        print(topic)
-    }
-    
-    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16) {
-        print("string", message.string)
-    }
-    
-    func mqttDidPing(_ mqtt: CocoaMQTT) {
-        print(mqtt.description)
-    }
-    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
-        print(mqtt.description)
-    }
-    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
-        print("error")
-        print(err.debugDescription)
-        self.mqtt?.connect()
-    }
-    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
-        print("aqui4")
-    }
-    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
-        print("aqui5")
-    }
-    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
-        print("aqui6")
-    }
-}
-
