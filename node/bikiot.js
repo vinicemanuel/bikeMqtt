80891463ece875165aa0ef0ebb45f44643754e54
José Murilo <30482984+jmsmf@users.noreply.github.com>
2018-12-02 23:27:45 -0300

    adding commentary to the code

--- node/bikiot.js
+++ node/bikiot.js
@@ -2,27 +2,27 @@ let Gpio = require('onoff').Gpio
 let mqtt = require('mqtt')
 let usonic = require('mmm-usonic')
 
-let client = mqtt.connect('mqtt://broker.hivemq.com')
+let client = mqtt.connect('mqtt://broker.hivemq.com') //mqtt broker connection
 
-let rotation = new Gpio(4, 'in', 'falling', {debounceTimeout: 10})
+let rotation = new Gpio(4, 'in', 'falling', {debounceTimeout: 10}) //declares wire that works as current sensor for wheel rotation counting
 //let turnLeft = new Gpio(26, 'in', 'both', {debounceTimeout: 10})
 //let turnRight = new Gpio(27, 'in', 'both', {debounceTimeout: 10})
 //let ledLeft = new Gpio(24, 'out')
 //let ledRight = new Gpio(23, 'out')
-let led = new Gpio(17, 'out')
+let led = new Gpio(17, 'out') //declares wire responsable for reacting to the proximity sensor
 
 let vcc = new Gpio(18, 'out')
 vcc.write(1, function() {})
 
-let counter = 0
+let counter = 0 //wheel turn counter
 //let makingALeft = false
 //let makingARight = false
-let resposta = {}
+let resposta = {} //object used to send data
 
 //let leftTurnInterval, rightTurnInterval
-let mqttInterval = setInterval(function(){sendMqtt();}, 2000)
+let mqttInterval = setInterval(function(){sendMqtt();}, 2000) // defines the interval in which the mqtt data is published
 
-function sendMqtt() {
+function sendMqtt() { //function to send mqtt data
 	resposta.counter = counter
 	resposta.timestamp = new Date()
 	counter = 0
@@ -30,7 +30,7 @@ function sendMqtt() {
 	client.publish('bikeiot', JSON.stringify(resposta))
 }
 
-function makeALeft(){
+function makeALeft(){ //code for the turning button, not finished during the hackathon
 	makingALeft = true
 	makingARight = false
 	leftTurnInterval = setInterval(function(){
@@ -41,7 +41,7 @@ function makeALeft(){
 	}, 500)
 }
 
-function makeARight(){
+function makeARight(){ //code for the turning button, not finished during the hackathon
 	makingARight = true
 	makingLeft = false
 	console.log(ledRight.readSync())
@@ -53,23 +53,23 @@ function makeARight(){
 	}, 500)
 }
 
-rotation.watch((err, value) => {
+rotation.watch((err, value) => { //function that reacts to the state change in the makeshift current sensor used for counting wheel turns
 	if (err) 
 		throw err
 	counter++
 })
 
-usonic.init(function (error) {
+usonic.init(function (error) { //initialization of the ultrasonic proximity sensor
 	if (error) {
 		console.log(error)
 		return
 	}
-	let sensor = usonic.createSensor(21, 20, 1000)
+	let sensor = usonic.createSensor(21, 20, 1000) //defining the proximity sensor
 	setInterval(function() {
 		if(sensor().toFixed(2) < 120){
-			led.write(1, function() {})
+			led.write(1, function() {}) // turns on leds and buzzer if distance from the sensor is under 120 cm
 		} else {
-			led.write(0, function(){})
+			led.write(0, function() {}) // turns off leds and buzzer if distance from the sensor is above 120 cm
 		}
 	}, 200)
 })
